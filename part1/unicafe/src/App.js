0c382755bb8c186b19c4c6416380ac4946adf90d
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-24 14:09:22 -0800

    exercise 1.9 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,6 +11,11 @@ const Display = (props) => <div><h1>{props.text}</h1></div>
 const Statistics = (props) => {
   const sum = props.good + props.neutral + props.bad
   
+  // if no feedback given then give no feedback message instead of statistics
+  if (sum === 0) {
+    return <div><p>No feedback given</p></div>
+  }
+
   return(
     <div>
       <p>
@@ -18,8 +23,8 @@ const Statistics = (props) => {
         neutral {props.neutral} <br />
         bad {props.bad} <br />
         all {sum} <br />
-        average {(props.good - props.bad) / sum || 0} <br />
-        positive {props.good/sum * 100 || 0}% <br />
+        average {(props.good - props.bad) / sum} <br />
+        positive {props.good/sum * 100}% <br />
       </p>
     </div>
   )
@@ -31,6 +36,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  //clicking each button increasing its state counter by 1
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
   const handleBadClick = () => setBad(bad + 1)
