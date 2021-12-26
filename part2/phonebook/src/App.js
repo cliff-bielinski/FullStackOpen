98d204586cdd209b9099a0b82264e4d290a50aad
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-26 13:52:43 -0800

    exercise 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,7 @@ const Display = ({ title }) => <h2>{title}</h2>
 
 const Person = ({ person }) => <div>{person.name}: {person.number}</div>
 
-const Numbers = ({ persons }) => {
-  return(  
-    <div>
-      <Display title="Numbers" />
-      {persons.map(person => <Person key={person.id} person={person} />)}
-    </div>
-  )
-}
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-7890', id: 1}
-  ]) 
+const Add = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -45,17 +33,59 @@ const App = () => {
 
   return (
     <div>
-      <Display title='Phonebook' />
+      <Display title="Add a person" />
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons}/>
+    </div>
+  )
+}
+
+const Filter = ({ filter, setFilter }) => {
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  return(
+    <form>
+      <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
+    </form>
+  )
+}
+
+const Numbers = ({ persons, filter }) => {
+  const phonebook = filter
+    ? persons.filter(person => 
+      person.name
+        .toLowerCase()
+        .startsWith(filter.toLowerCase()))
+    : persons
+
+  return(  
+    <div>
+      <Display title="Numbers" />
+      {phonebook.map(person => <Person key={person.id} person={person} />)}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [filter, setFilter] = useState('')
+  
+  return (
+    <div>
+      <Display title='Phonebook' />
+      <Filter filter={filter} setFilter={setFilter} />
+      <Add persons={persons} setPersons={setPersons} />
+      <Numbers persons={persons} filter={filter} />
     </div>
   )
 }
