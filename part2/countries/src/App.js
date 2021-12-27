b29f61e4e0676d18f9c0ab1623f5b3e22895d263
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-27 12:25:30 -0800

    exercises 2.12 done

---
+++ part2/countries/src/App.js
@@ -0,0 +1,90 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+/* 
+  Handles user input for searching countries by name.
+  Sets user input to 'filter' state which is used by
+  Display to filter out country results.
+*/
+const Search = ({ filter, setFilter, handleSearch }) => {
+  return (
+    <form>
+      <div>find countries 
+        <input value={filter} onChange={handleSearch} />
+      </div>
+    </form>
+  )
+}
+
+const Results = ({ countries , filter }) => {
+  
+  const results = countries.filter(country => 
+    country.name.common
+    .toLowerCase()
+    .includes(filter.toLowerCase())
+  )
+  if (!filter || !results.length) return <div>No results</div>
+  
+  else if (results.length == 1) return <Display country={results[0]}/>
+
+  else if (results.length <= 10) {
+    return (
+      <>
+        {results.map(result => 
+          <div key={result.name.common}>{result.name.common}</div>
+        )}
+      </>
+    )
+  }
+
+  else return <div>Too many matches</div>
+}
+
+const Display = ({ country }) => {
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>
+        Capital: {country.capital} <br />
+        Population: {country.population}
+      </div>
+      <h2>Languages</h2>
+      <ul>
+        {Object.entries(country.languages).map(language =>
+          <li key={language[0]}>{language[1]}</li>
+        )}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
+  )
+}
+
+function App() {
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  const handleSearch = (event) => setFilter(event.target.value)
+
+  // gets country data from REST Countries API
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Search 
+        filter={filter} 
+        setFilter={setFilter} 
+        handleSearch={handleSearch} 
+      />
+      <Results countries={countries} filter={filter} />
+    </div>
+  );
+}
+
+export default App;
