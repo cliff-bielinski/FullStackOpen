44972bae67154c7aeb7befad2f0cc061d202286a
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-28 12:40:48 -0800

    exercise 2.15 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Display = ({ title }) => <h2>{title}</h2>
 
@@ -16,10 +16,13 @@ const Add = (props) => {
     else {
       const personObject = {
         name: props.newName,
-        number: props.newNumber,
-        id: props.persons.length + 1
+        number: props.newNumber
       }
-      props.setPersons(props.persons.concat(personObject))
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          props.setPersons(props.persons.concat(returnedPerson))
+        })
     }
 
     props.setNewName('')
@@ -75,10 +78,10 @@ const App = () => {
   const handleFilterChange = (event) => setFilter(event.target.value)
   
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
