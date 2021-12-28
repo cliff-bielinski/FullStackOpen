ee29733830db2ae8ca9fd4be24e89d422f144bf3
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-28 13:45:24 -0800

    remove function added in services

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -12,6 +12,11 @@ const create = newObject => {
     return request.then(response => response.data)
 }
 
+const remove = (id) => {
+    const request = axios.delete(`${baseURL}/${id}`)
+    return request.then(response => response.data)
+}
+
 const update = (id, newObject) => {
     const request = axios.put(`${baseURL}/${id}`, newObject)
     return request.then(response => response.data)
