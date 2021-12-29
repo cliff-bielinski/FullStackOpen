268b318dbdbb4648dc25bc9ac435faffa6a39f9c
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-29 14:42:21 -0800

    exercise 3.3 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -30,6 +30,20 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+// retrieves specific contact for a given id
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(`Fetching contact with id: ${id}`)
+  const person = persons.find(person => person.id === id)
+  
+  // returns matching contact or 404 if not found
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 // retrieves total number of contacts in phonebook and current server time
 app.get('/info', (request, response) => {
   let info = `
@@ -41,7 +55,7 @@ app.get('/info', (request, response) => {
   response.send(info)
 })
 
-// server runs locally :3001
+// server runs locally on port 3001
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
