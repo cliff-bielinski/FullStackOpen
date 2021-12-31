f57e37e73e29ea344829b2dbb3723c972a63c5f6
Cliff Bielinski <bielinski.cm@gmail.com>
2021-12-30 22:27:43 -0800

    exercise 3.9 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // HTTP request logging in terminal
 morgan.token('content', (request, response) => {
