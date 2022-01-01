c9b3aca7971cf57c4e8bdd1f0f30f928a64bec93
Cliff Bielinski <bielinski.cm@gmail.com>
2022-01-01 15:59:33 -0800

    controller file refactored

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,37 +1,20 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
