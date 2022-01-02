6a092d869461635528190dfb5088e8fcafff9c1d
Cliff Bielinski <bielinski.cm@gmail.com>
2022-01-01 17:51:47 -0800

    exercise 4.4 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const reducer = (sum, blogPost) => sum + blogPost.likes
+
+  return blogs.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
